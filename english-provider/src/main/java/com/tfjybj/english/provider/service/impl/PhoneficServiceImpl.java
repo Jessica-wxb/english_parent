f31944ff39fd0a2fae41031a28f35a0eaeae907b
yanweiqiang <ywq1016243402@163.com>
2019-06-15 10:38:23 +0800

    2019年6月15日10:33:51--闫伟强--音标查询全部

--- english-provider/src/main/java/com/tfjybj/english/provider/service/impl/PhoneficServiceImpl.java
+++ english-provider/src/main/java/com/tfjybj/english/provider/service/impl/PhoneficServiceImpl.java
@@ -153,5 +153,14 @@ public class PhoneficServiceImpl extends BaseServicePlusImpl<PhoneficDao, Phonef
         return false;
     }
 
+    /**
+     * 音标练习(看)-查询所有音标
+     * @Author 闫伟强
+     * @return 音标对应图片
+     */
+    @Override
+    public List<PhoneficModel> getPhonefic() {
 
+        return phoneficDao.getPhonefic();
+    }
 }
