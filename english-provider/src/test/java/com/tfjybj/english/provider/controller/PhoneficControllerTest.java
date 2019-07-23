9262c6d55b0fa3a11dfb220f4d21b51e28f6970a
limit <myylimit@163.com>
2019-07-23 11:48:34 +0800

    添加test构建方式--2019-7-17 20:44:15-马莹

--- english-provider/src/test/java/com/tfjybj/english/provider/controller/PhoneficControllerTest.java
+++ english-provider/src/test/java/com/tfjybj/english/provider/controller/PhoneficControllerTest.java
@@ -1,31 +1,26 @@
 package com.tfjybj.english.provider.controller;
 
-import com.tfjybj.english.model.WordModel;
 import com.tfjybj.english.provider.until.ContrastImgUntil;
 import net.coobird.thumbnailator.Thumbnails;
 import org.junit.Test;
 
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
-import static org.junit.Assert.*;
 
 public class PhoneficControllerTest {
 
     @Test
     public void testFingerPrint() throws IOException {
-        Thumbnails.of(new File("C:\\Users\\Limit\\Desktop\\img\\roseTwo.jpg")).size(300, 300).toFile("../rose.jpg");
+        Thumbnails.of(new File("C:\\Users\\Limit\\Desktop\\img\\timg.jpg")).size(300, 300).toFile("../rose.jpg");
 
-        Thumbnails.of(new File("C:\\Users\\Limit\\Desktop\\img\\roseOne.jpg")).size(300, 300).toFile("../rose1.jpg");
+        Thumbnails.of(new File("C:\\Users\\Limit\\Desktop\\img\\timgqqweqweqwe.jpg")).size(300, 300).toFile("../rose1.jpg");
         ContrastImgUntil fp1 = new ContrastImgUntil(ImageIO.read(new File("../rose.jpg")));
         ContrastImgUntil fp2 = new ContrastImgUntil(ImageIO.read(new File("../rose1.jpg")));
-//        System.out.println(fp1.toString(true));
-        System.out.printf(fp1.compare(fp2) + "");
-
-        System.out.println("");
+        System.out.println("图片1二进制码:" + "\r\n" + fp1.toString(true));
+        System.out.println("图片2二进制码:" + "\r\n" + fp2.toString(true));
+        System.out.printf("图片相似率" + "\r\n" + fp1.compare(fp2) + "\r\n" +"");
     }
 
 
